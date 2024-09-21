c53ef470bb1fc294a9923769c9b8343d84796497
Marcos Derlei Meyer <marcos.derlei85@gmail.com>
2024-09-21 15:58:26 -0300

    Bad Solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,13 +39,15 @@ public class Program {
 				System.out.println("Error in reservation: Reservation dates for update must be future dates");
 			} else if (!checkOut.after(checkIn)) {
 				System.out.println("Error in reservation: Check-out date must be after check-in date");
-			} else {
-				reservation.updateDates(checkIn, checkOut);
-				System.out.println("Reservation: " + reservation);
+				String error = reservation.updateDates(checkIn, checkOut);
+				if (error != null) {
+					System.out.println("Error in reservation: " + error);
+				} else {
+					reservation.updateDates(checkIn, checkOut);
+					System.out.println("Reservation: " + reservation);
+				}
 			}
 		}
-
 		sc.close();
 	}
-
 }
